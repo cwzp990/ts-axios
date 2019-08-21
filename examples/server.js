bb497089279208d5f650eb6c7b4f2ea0f694562c
cwzp990 <cwzp990@gmail.com>
2019-08-21 10:35:49 +0800

    响应数据泛型

--- examples/server.js
+++ examples/server.js
@@ -107,6 +107,17 @@ router.patch('/extend/patch', function (req, res) {
   res.json(req.body)
 })
 
+router.get('/extend/user', function(req,res) {
+  res.json({
+    code: 0,
+    message: 'ok',
+    result: {
+      name: 'jack',
+      age: 18
+    }
+  })
+})
+
 app.use(router)
 
 const port = process.env.PORT || 8080
