6811490ab0b1637df3e53595a13f2a4d151b8d20
cwzp990 <cwzp990@gmail.com>
2019-09-02 17:11:12 +0800

    test(util.spec.ts): write utils test

--- test/helpers/util.spec.ts
+++ test/helpers/util.spec.ts
@@ -1,4 +1,4 @@
-import { isDate, isPlainObject, extend } from '../../src/helpers/utils'
+import { isDate, isPlainObject, extend, deepMerge } from '../../src/helpers/utils'
 
 describe('helpers:util', () => {
   describe('isXX', () => {
@@ -27,6 +27,30 @@ describe('helpers:util', () => {
       const c = extend(a, b)
       expect(c.foo).toBe(123)
       expect(c.bar).toBe(789)
+    })
+  })
+
+  describe('deepMerge', () => {
+    test('should be immutable', () => {
+      const a = Object.create(null)
+      const b: any = { foo: 123 }
+      const c: any = { bar: 456 }
+      const d = deepMerge(a, b, c)
+
+      expect(typeof a.foo).toBe('undefined')
+      expect(typeof a.bar).toBe('undefined')
+      expect(typeof b.bar).toBe('undefined')
+      expect(typeof c.foo).toBe('undefined')
+    })
+
+    test('should deepMerge properties', () => {
+      const a = { foo: 123 }
+      const b = { bar: 456 }
+      const c = { foo: 789 }
+      const d = deepMerge(a, b, c)
+
+      expect(d.foo).toBe(789)
+      expect(d.bar).toBe(456)
     })
   })
 })
