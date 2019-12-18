85126f75dc93725d90d647c7f7206021e7b2c0b3
cwzp990 <cwzp990@gmail.com>
2019-12-18 16:56:59 +0800

    类型检查文件导出

---
+++ src/core/axios.ts
@@ -0,0 +1,41 @@
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types/index'
+import xhr from './xhr'
+import { buildUrl } from '../helpers/url'
+import { transformRequest, transformResponse } from '../helpers/data'
+import { processHeaders } from '../helpers/headers'
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config) // 对配置参数进行处理
+  return xhr(config) // 发送http请求 响应Promise化
+    .then(res => {
+      return transformResponseData(res) // 响应数据默认是JSON字符串，需要将它转换成对象方便后续调用
+    })
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config) // 对url参数进行处理
+  config.headers = transformHeaders(config) // 对headers进行处理
+  config.data = transformRequestData(config) // 对请求data做处理，对象需转换为JSON字符串
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildUrl(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  const { data } = res
+  return transformResponse(data)
+}
+
+export default axios
