a59b7e0beda54dd406052e6949e2092ec33eaf16
cwzp990 <cwzp990@gmail.com>
2019-12-25 17:16:29 +0800

    源码注释

--- src/axios.ts
+++ src/axios.ts
@@ -9,9 +9,16 @@ import Cancel, { isCancel } from './cancel/cancel'
 // 工厂函数
 function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config) // 实例化
+  // 这样instance就指向了request方法，且上下文指向context，所以可以直接以 instance(option) 方式调用
+  // Axios.prototype.request 内对第一个参数的数据类型判断，使我们能够以 instance(url, option) 方式调用
   const instance = Axios.prototype.request.bind(context)
 
-  // axios属性需要拷贝到instance上
+  // 把Axios.prototype上的方法扩展到instance对象上
+  // 这样 instance 就有了 get、post、put等方法
+  // 并指定上下文为context，这样执行Axios原型链上的方法时，this会指向context
+  // 把context对象上的自身属性和方法扩展到instance上
+  // 注：因为extend内部使用的forEach方法对对象做for in遍历时，只遍历对象本身的属性，而不会遍历原型链上的属性
+  // 这样，instance就有了defaults、interceptors属性
   extend(instance, context)
   return instance as AxiosStatic
 }
--- src/core/axios.ts
+++ src/core/axios.ts
@@ -26,10 +26,11 @@ export default class Axios {
 
   // 初始化拦截器
   // 配置参数由外部传入
+  // 用户调用axios.interceptors.request.use添加拦截器，当用户发起一个请求时，会依次执行拦截器数组里的拦截器
   constructor(initConfig: AxiosRequestConfig) {
     this.defaults = initConfig
     this.interceptors = {
-      request: new interceptorManagers<AxiosRequestConfig>(), // 用户调用axios.interceptors.request.use添加拦截器
+      request: new interceptorManagers<AxiosRequestConfig>(),
       response: new interceptorManagers<AxiosResponse>()
     }
   }
@@ -50,6 +51,7 @@ export default class Axios {
 
     config = mergeConfig(this.defaults, config) // 合并配置
 
+    // 中间件
     const chain: PromiseChain<any>[] = [
       {
         resolved: dispatchRequest,
@@ -69,6 +71,17 @@ export default class Axios {
 
     let promise = Promise.resolve(config)
     while (chain.length) {
+      // config会按序通过请求拦截器 dispatchRequest 响应拦截器
+      // 数组的 shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+      // 每次执行while循环，从chain数组里按序取出两项，并分别作为promise.then方法的第一个和第二个参数
+      // 按照我们使用InterceptorManager.prototype.use添加拦截器的规则，正好每次添加的就是我们通过InterceptorManager.prototype.use方法添加的成功和失败回调
+      // 通过InterceptorManager.prototype.use往拦截器数组里添加拦截器时使用的数组的push方法，
+      // 对于请求拦截器，从拦截器数组按序读到后是通过unshift方法往chain数组数里添加的，又通过shift方法从chain数组里取出的，所以得出结论：对于请求拦截器，先添加的拦截器会后执行
+      // 对于响应拦截器，从拦截器数组按序读到后是通过push方法往chain数组里添加的，又通过shift方法从chain数组里取出的，所以得出结论：对于响应拦截器，添加的拦截器先执行
+      // 第一个请求拦截器的fulfilled函数会接收到promise对象初始化时传入的config对象，而请求拦截器又规定用户写的fulfilled函数必须返回一个config对象，所以通过promise实现链式调用时，每个请求拦截器的fulfilled函数都会接收到一个config对象
+      // 第一个响应拦截器的fulfilled函数会接受到dispatchRequest（也就是我们的请求方法）请求到的数据（也就是response对象）,而响应拦截器又规定用户写的fulfilled函数必须返回一个response对象，所以通过promise实现链式调用时，每个响应拦截器的fulfilled函数都会接收到一个response对象
+      // 任何一个拦截器的抛出的错误，都会被下一个拦截器的rejected函数收到，所以dispatchRequest抛出的错误才会被响应拦截器接收到。
+      // 因为axios是通过promise实现的链式调用，所以我们可以在拦截器里进行异步操作，而拦截器的执行顺序还是会按照我们上面说的顺序执行，也就是 dispatchRequest 方法一定会等待所有的请求拦截器执行完后再开始执行，响应拦截器一定会等待 dispatchRequest 执行完后再开始执行
       const { resolved, rejected } = chain.shift()!
       promise = promise.then(resolved, rejected)
     }
@@ -107,7 +120,7 @@ export default class Axios {
 
   getUri(config?: AxiosRequestConfig): string {
     config = mergeConfig(this.defaults, config)
-    return transformURL(config)
+    return transformURL(config) // buildURL
   }
 
   _requestMethodWithoutData(
